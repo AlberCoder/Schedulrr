1ec3ee4150fdeef46fc2c26bd6c85976546a97ea
Alber Ahmed <isalberahmed@gmail.com>
2024-10-05 09:51:08 +0500

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,14 @@
 export default function NotFound() {
-  return (
-    <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
-}
+    return (
+        <div className="min-h-screen flex flex-col justify-center  items-center bg-gradient-to-b from-blue-50 to-white">
+      <h1 className="text-6xl font-bold text-blue-600">404</h1>
+      <p className="mt-4 text-gray-600 text-lg md:text-xl  lg:text-2xl text-center mx-4">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+
+      <a href="/" className="mt-6 bg-blue-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-600 text-center">
+        Go Back Home
+      </a>
+      </div>
+    );
+  }
